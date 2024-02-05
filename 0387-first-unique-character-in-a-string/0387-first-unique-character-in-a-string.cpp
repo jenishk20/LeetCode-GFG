0485025cb5d61639eaac015c448ac2a488e0042c
jenishk20 <60069162+jenishk20@users.noreply.github.com>
2024-02-05 20:21:41 +0530

    Time: 67 ms (7.43%), Space: 13.1 MB (10.77%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int>ma;
+        for(auto i:s) ma[i]++;
+        
+        for(int i=0;i<s.size();i++){
+            if(ma[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
