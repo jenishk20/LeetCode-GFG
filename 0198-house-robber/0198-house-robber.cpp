b68ce98a12b2a37460a2e1d3fd6700c8d816b64d
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-04-23 20:00:18 +0530

    Time: 4 ms (32.46%), Space: 9.3 MB (10.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -18,14 +18,18 @@ public:
         
         dp[0] = nums[0];
         
+        int prev1 = nums[0];
+        int prev2 = 0;
+        
         for(int i=1;i<n;i++){
             
-            int op1 = (i>=2)?dp[i-2] + nums[i]:nums[i];
-            int op2 = dp[i-1];
-            
-            dp[i] = max(op1,op2);
+            int op1 = (i>=2)?prev2 + nums[i]:nums[i];
+            int op2 = prev1;
+        
+            prev2 = prev1;
+            prev1 = max(op2,op1);
         }
-        return dp[n-1];
+        return prev1;
         
         
         
