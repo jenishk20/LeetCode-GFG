d0765ba63b0ab3eb2361854d414d708e04eaf21b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-04-28 05:27:52 +0530

    Time: 4 ms (93.69%), Space: 10 MB (78.83%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -1,25 +1,54 @@
 class Solution {
-    public:
-   int dp[101][101];
-    int solve(int ptr, int index, string& ring, string& key, int n, int m) {
-        if (index >= m)
-            return 0;
-        if (dp[ptr][index] != -1)
-            return dp[ptr][index];
 
-        int steps = INT_MAX;
-        for (int i = 0; i < ring.size(); i++)
-            if (ring[i] == key[index])
-                steps = min(steps, min(abs(i - ptr), n - abs(i - ptr)) + 1 +
-                                       solve(i, index + 1, ring, key, n, m));
-        return dp[ptr][index] = steps;
+private:
+    map<char,vector<int>>positionsOfChars;
+    int dp[201][201];
+    
+    int getMinSpells(int ringPointer,int keyPointer,int n,int m,string &ring,string &key){
+        
+        if(keyPointer>=m || ringPointer>=n) return 0;
+        
+        // Check if sub-problem is already solved
+        
+        if(dp[ringPointer][keyPointer]!=-1) return dp[ringPointer][keyPointer];
+        
+        // Iterate through each of the occurences of ring[ringPointer]
+        
+        int minSteps = INT_MAX;
+        
+        for(auto idx : positionsOfChars[key[keyPointer]]){
+            
+            int clockWiseDist = abs(idx-ringPointer);
+            int antiClockWiseDist = n-abs(idx-ringPointer);
+            
+
+            int minPossDist = min(clockWiseDist,antiClockWiseDist);
+            
+            int stepsTaken =  minPossDist + 
+                getMinSpells(idx,keyPointer+1,n,m,ring,key) +1;
+            
+            minSteps = min(minSteps,stepsTaken);
+            
+            
+        }
+        
+        return dp[ringPointer][keyPointer] = minSteps;
     }
+    
+public:
     int findRotateSteps(string ring, string key) {
         
-         int n = ring.size();
+        int n = ring.size();
         int m = key.size();
-        memset(dp, -1, sizeof(dp));
-
-        return solve(0, 0, ring, key, n, m);
+        
+        for(int i=0;i<ring.size();i++){
+            positionsOfChars[ring[i]].push_back(i);
+        }
+        
+        int ringPointer=0,keyPointer=0;
+        
+        memset(dp,-1,sizeof dp);
+        
+        return getMinSpells(ringPointer,keyPointer,n,m,ring,key);
     }
 };
