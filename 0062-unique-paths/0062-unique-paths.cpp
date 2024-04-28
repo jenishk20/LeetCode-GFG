5d006eef8732e31632aeb4aabdb201d8ca8a38a0
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-04-28 18:08:28 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (68.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    int dp[201][201];
-    int recur(int i,int j,int m,int n){
-        if(i<0 || j<0) return 0;
-        
-        if(i==0 && j==0) return 1;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int op1 = recur(i-1,j,m,n);
-        int op2 = recur(i,j-1,m,n);
-        
-        return dp[i][j] = op1+op2;
-    }
+
     int uniquePaths(int m, int n) {
-    
-        for(int i=0;i<m;i++){
+        
+        vector<int>dp(n,0);
+        
+        int i,j;
+        
+        
+        
+        for(i=0;i<m;i++){
             
-            for(int j=0;j<n;j++){
+            vector<int>temp(n,0);
+            
+            for(j=0;j<n;j++){
                 
-                if(i==0 && j==0) {dp[i][j] = 1; continue;}
+                if(i==0 && j==0) {
+                    temp[j] = 1;
+                    continue;
+                }
                 
-                int op1 = 0,op2 = 0;
+                int op1 = 0, op2 = 0;
                 
-                if(i-1>=0) op1 = dp[i-1][j];
-                if(j-1>=0) op2 = dp[i][j-1];
+                if(i-1>=0) op1 = dp[j];
+                if(j-1>=0) op2 = temp[j-1];
                 
-                dp[i][j] = op1 + op2;
+                temp[j] = op1 + op2;
+                
             }
+            dp = temp;
         }
-        return dp[m-1][n-1];
+        
+        return dp[n-1];
     }
 };
