48de45d8ae94c05c43703e364d5a60361d36aa0e
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-04-28 18:02:25 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (71.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,8 +14,21 @@ public:
         return dp[i][j] = op1+op2;
     }
     int uniquePaths(int m, int n) {
-        
-        memset(dp,-1,sizeof dp);
-        return recur(m-1,n-1,m,n);
+    
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(i==0 && j==0) {dp[i][j] = 1; continue;}
+                
+                int op1 = 0,op2 = 0;
+                
+                if(i-1>=0) op1 = dp[i-1][j];
+                if(j-1>=0) op2 = dp[i][j-1];
+                
+                dp[i][j] = op1 + op2;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
