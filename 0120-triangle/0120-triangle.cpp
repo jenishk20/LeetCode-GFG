988f190648466d5cec7be252cef74b1b4fc11e2e
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-04-28 19:18:41 +0530

    Time: 2 ms (88.37%), Space: 11.1 MB (71.83%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -16,7 +16,25 @@ public:
         
         int n = triangle.size();
         
-        memset(dp,-1,sizeof dp);
-        return recur(0,0,n,triangle);
+        // memset(dp,-1,sizeof dp);
+        
+        for(int i=n-1;i>=0;i--){
+            
+            for(int j=0;j<triangle[i].size();j++){
+                
+                if(i==n-1){
+                    dp[i][j] = triangle[i][j];
+                    continue;
+                }
+                
+                int op1 = triangle[i][j] + dp[i+1][j];
+                int op2 = triangle[i][j] + dp[i+1][j+1];
+                
+                dp[i][j] = min(op1,op2);
+        
+            }
+        }
+        
+        return dp[0][0];
     }
 };
