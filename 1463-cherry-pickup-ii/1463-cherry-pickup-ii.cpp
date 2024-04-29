a94c07c984ddf31b9e755325ce883f143988bc8f
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-04-29 19:12:24 +0530

    Time: 46 ms (65.68%), Space: 15.5 MB (73.40%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,60 +1,49 @@
 class Solution {
 public:
-    
+    int dp[101][101][101];
+    int recur(int i,int j1,int j2,vector<vector<int>>&grid,int n,int m){
+        
+        if(j1<0 || j1>=m || j2<0 || j2>=m) return -1e8;
+        
+        if(i==n-1){
+            
+            if(j1==j2) return grid[i][j1];
+            
+            return grid[i][j1] + grid[i][j2];
+        }
+        
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        
+        int maxCherries = 0;
+        
+        for(int dx = -1;dx<=1;dx++){
+            for(int dy = -1;dy<=1;dy++){
+                
+                int newPosX = j1 + dx;
+                int newPosY = j2 + dy;
+                
+               
+                int op1 = 0;
+                
+                if(j1==j2){
+                    op1 = grid[i][j1] + recur(i+1,newPosX,newPosY,grid,n,m);    
+                }
+                else{
+                    op1 = grid[i][j1] + grid[i][j2] + recur(i+1,newPosX,newPosY,grid,n,m);  
+                }
+                
+                maxCherries = max(maxCherries,op1);
+            }
+        }
+        return dp[i][j1][j2] = maxCherries;
+        
+    }
     int cherryPickup(vector<vector<int>>& grid) {
         
-        int r=grid.size();
-        int c=grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
+        memset(dp,-1,sizeof dp);
         
-        vector<vector<int>>curr(c,vector<int>(c,0));
-        vector<vector<int>>next(c,vector<int>(c,0));
-        
-        int i,j1,j2;
-        for(j1=0;j1<c;j1++)
-        {
-            for(j2=0;j2<c;j2++)
-            {
-                if(j1==j2)
-                    next[j1][j2]=grid[r-1][j1];
-                else
-                    next[j1][j2]=grid[r-1][j1]+grid[r-1][j2];
-            }
-        }
-        
-        for(i=r-2;i>=0;i--)
-        {
-            for(j1=0;j1<c;j1++)
-            {
-                for(j2=0;j2<c;j2++)
-                {
-                   
-                     int maxi=-1e8;
-                    for(int dx=-1;dx<=1;dx++)
-                    {
-                        for(int dy=-1;dy<=1;dy++)
-                        {
-                            int value=0;
-                            if(j1==j2)
-                            {
-                                value=grid[i][j1];
-                            }
-                            else
-                                value+=grid[i][j1]+grid[i][j2];
-                            if(j1+dx>=0 and j2+dy>=0 and j1+dx<c and j2+dy<c)
-                            value+=next[j1+dx][j2+dy];
-                            else
-                            value+=(-1e8);
-                            maxi=max(maxi,value);
-                        }
-                    }
-                    curr[j1][j2]=maxi;
-                }
-
-            }
-            next=curr;
-        }
-        return next[0][c-1];
-        // return recur(0,0,c-1,r,c,grid,dp);
-        
+        return recur(0,0,m-1,grid,n,m);
     }
 };
