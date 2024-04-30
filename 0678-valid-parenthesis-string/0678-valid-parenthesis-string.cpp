d9f7401b455312ff5847f039466c45e7ab37cde3
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-01 05:22:29 +0530

    Time: 20 ms (5.05%), Space: 30.5 MB (5.00%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
+    
+    int dp[101][101];
+    bool recur(int idx,int openCount,string s,int n){
+        if(idx==n) return openCount == 0;
+        
+        bool ans = false;
+        
+        if(dp[idx][openCount]!=-1) return dp[idx][openCount];
+        
+        if(s[idx] == '*') {
+            
+            int op1 = recur(idx+1,openCount+1,s,n);
+            int op2 = openCount>0?recur(idx+1,openCount-1,s,n):0;
+            int op3 = recur(idx+1,openCount,s,n);
+            
+            ans |= op1 | op2 | op3;
+        }
+        else if(s[idx] =='('){
+            ans = ans | recur(idx+1,openCount+1,s,n);
+        }
+        else{
+            ans = ans | openCount>0?recur(idx+1,openCount-1,s,n):0;
+        }
+        return dp[idx][openCount] = ans;
+    }
     bool checkValidString(string s) {
         
-        int n = s.size();
-        
-        int leftMin = 0, leftMax =0;
-        
-        int ans = 0;
-        
-        int i;
-        
-        for(i=0;i<n;i++){
-            
-            if(s[i]=='('){
-                leftMin++;
-                leftMax++;
-            }
-            else if(s[i]==')'){
-                leftMin--;
-                leftMax--;
-            }
-            else{
-                
-                leftMin--;
-                leftMax++;
-            }
-            
-            if(leftMax<0) return false;
-            
-            leftMin = max(0,leftMin);
-        }
-        
-        return leftMin == 0;
+        memset(dp,-1,sizeof dp);
+        return recur(0,0,s,s.size());
     }
 };
