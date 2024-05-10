15d061078320caf115b7f86ddb90ec6a848cbbbe
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-10 08:36:20 +0530

    Time: 260 ms (64.80%), Space: 10.9 MB (86.85%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    vector<int> findNoSmallerThanMid(double mid,vector<int>&arr,int k){
+        
+        int i=0,j;
+        int n = arr.size();
+        
+        int cnt =0;
+        int num = arr[0];
+        int den = arr[n-1];
+        
+        for(j=1;j<n;j++){
+            
+            cout<<arr[i]<<" "<<arr[j]<<" "<<mid<<" "<<cnt<<endl;
+            while(i<j and arr[i] <= mid*arr[j]){
+                i++;
+            }
+            
+            cnt+=i;
+            
+            if(i-1>=0 && arr[i-1]*den>num*arr[j]){
+                num = arr[i-1];
+                den = arr[j];
+            }
+        }
+        return {cnt,num,den};
+        
+    }
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        
+        double low = 0;
+        double high = 1;
+        
+        while(low<high){
+            
+            double mid = (low+high)/2;
+            
+            int cnt = 0;
+            
+            vector<int>res = findNoSmallerThanMid(mid,arr,k);
+            cout<<res[0]<<endl;
+            if(res[0] == k){
+                return {res[1],res[2]};
+            }
+            if(res[0]<k){
+                low = mid;
+            }
+            else{
+                high = mid;
+            }
+            
+        }
+        return {};
+    }
+};
