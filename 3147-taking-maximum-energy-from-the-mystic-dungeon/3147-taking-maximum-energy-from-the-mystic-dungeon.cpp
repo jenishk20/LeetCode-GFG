58812ddcf16d17691ff1d0fa906ecd890048d460
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-13 03:41:18 +0530

    Time: 155 ms (50.00%), Space: 148.9 MB (100.00%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,31 +1,54 @@
+// class Solution {
+
+// private:
+//     int recur(int st,vector<int>&energy,int k){
+        
+//         if(st>=energy.size()) return 0;
+        
+//         return energy[st] + recur(st+k,energy,k);
+//     }
+      
+// public:
+
+//     int maximumEnergy(vector<int>& energy, int k) {
+        
+//         int n = energy.size();
+
+//         for(i=0;i<n;i++){
+//             ans = max(ans,recur(i,energy,k));
+//         }
+        
+//         return ans;  
+//     }
+// };
+
+
 class Solution {
+
+private:
+    int dp[100001];
+    int recur(int st,vector<int>&energy,int k){
+        
+        if(st>=energy.size()) return 0;
+        
+        if(dp[st]!=-1) return dp[st];
+        
+        return dp[st] = energy[st] + recur(st+k,energy,k);
+    }
+      
 public:
 
     int maximumEnergy(vector<int>& energy, int k) {
         
         int n = energy.size();
+        memset(dp,-1,sizeof dp);
         
-        int i;
-        int ans = -1e4;
-        vector<int>dp(n,0);
+        int ans = -1e5;
         
-        dp[n-1] = energy[n-1];
-        
-        for(i=n-2;i>=0;i--){
-            
-            if(i+k<n){
-                dp[i] = dp[i+k] + energy[i];
-            }
-            else{
-                dp[i] = energy[i];
-            }
+        for(int i=0;i<n;i++){
+            ans = max(ans,recur(i,energy,k));
         }
         
-        for(i=0;i<n;i++){
-            ans = max(ans,dp[i]);
-        }
-        
-        return ans;
-       
+        return ans;  
     }
 };
