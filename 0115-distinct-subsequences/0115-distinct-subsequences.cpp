fc436ce3747a85038bedd45e2fb983af9d653d4b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-14 20:39:01 +0530

    Time: 2289 ms (5.00%), Space: 420.9 MB (10.33%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int recur(int i,int j,string s,string t){
+        
+        if(j<0){
+            return 1;
+        }
+        
+        if(i<0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s[i]==t[j]){
+            int op1 = recur(i-1,j,s,t);
+            int op2 = recur(i-1,j-1,s,t);
+            return dp[i][j] = op1 + op2;
+        }
+        
+        return dp[i][j] =  recur(i-1,j,s,t);
+    }
+    int numDistinct(string s, string t) {
+        
+        int n = s.size();
+        int m = t.size();
+        memset(dp,-1,sizeof dp);
+        
+        return recur(n-1,m-1,s,t);
+    }
+};
