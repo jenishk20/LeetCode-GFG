2294bc87ce4fe92decd733bd83b61a7fc8737b2b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-17 08:39:18 +0530

    Time: 26 ms (5.46%), Space: 21.1 MB (98.58%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+//     TreeNode *recur(TreeNode *root,int target){
+        
+//         if(!root) return NULL;
+        
+//         if(!root->left and !root->right and root->val == target)
+//             return root;
+        
+//         TreeNode *left = recur(root->left,target);
+//         TreeNode *right = recur(root->right,target);
+        
+//         if(left && left->val == target) root->left = NULL;
+//         if(right && right->val == target) root->right = NULL;
+        
+//         if(left && right){
+//             cout<<root->val<<" "<<left->val<<" "<<right->val<<endl;
+//         }
+//         else if(left){
+//             cout<<root->val<<" "<<left->val<<" "<<"NOT PRESENT"<<endl;
+//         }
+//         else if(right){
+//             cout<<root->val<<" "<<"NOT PRESENT"<<" "<<right->val<<endl;
+//         }
+//         // cout<<root->val<<" "<<left->val<<" "<<right->val<<endl;
+//         return root;
+//     }
+    
+    bool recur(TreeNode *root,int target){
+        if(!root) return false;
+        
+        if(!root->left and !root->right and root->val == target)
+            return true;
+        
+        bool left = recur(root->left,target);
+        bool right = recur(root->right,target);
+        // cout<<"Inside\n";
+        if(left) root->left = NULL;
+        if(right) root->right = NULL;
+        cout<<root->val<<" "<<left<<" "<<right<<endl;
+        if(left and !root->right) {
+            return root->val == target;
+        }
+        else if(right and !root->left){
+            return root->val == target;
+        }
+        
+        return false;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        
+        bool ans = recur(root,target);
+        if(ans && root->val == target and !root->left
+          and !root->right) return NULL;
+        return root;
+    }
+};
