d3719ee5d486a9c9b0b5e63d0559da60c5441163
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-21 19:12:24 +0530

    Time: 1299 ms (5.01%), Space: 26.8 MB (40.70%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,19 +3,22 @@ public:
     
     int dp[2001][2001];
     bool recur(int i,int j,string &s,string &p){
-        if(j<0) return i<0;
         
-        if(i<0) {
-            for(int k=0;k<=j;k++){
-                if(p[k]!='*') return false;
+        if(i==0 and j==0) return true;
+        
+        if(i>0 and j==0) return false;
+ 
+        if(i==0 and j>0) {
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*') return false;
             }
             return true;
         }
         
         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(s[i] == p[j] || p[j] == '?') return dp[i][j] =  recur(i-1,j-1,s,p);
-        if(p[j] == '*') return dp[i][j] = recur(i-1,j,s,p) || recur(i,j-1,s,p);
+        if(s[i-1] == p[j-1] || p[j-1] == '?') return dp[i][j] =  recur(i-1,j-1,s,p);
+        if(p[j-1] == '*') return dp[i][j] = recur(i-1,j,s,p) || recur(i,j-1,s,p);
         
         return dp[i][j] = false;
     }
@@ -25,6 +28,6 @@ public:
         int m = p.size();
         
         memset(dp,-1,sizeof dp);
-        return recur(n-1,m-1,s,p);
+        return recur(n,m,s,p);
     }
 };
