da371326778205057373bd96498545e9f0812b0b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-21 19:17:46 +0530

    Time: 800 ms (5.01%), Space: 26.8 MB (40.70%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -27,7 +27,40 @@ public:
         int n = s.size();
         int m = p.size();
         
-        memset(dp,-1,sizeof dp);
-        return recur(n,m,s,p);
+        // memset(dp,-1,sizeof dp);
+        
+        int i,j;
+        
+        dp[0][0] = 1;
+        
+        for(i=1;i<=n;i++) dp[i][0] = 0;
+        
+        for(j=1;j<=m;j++){
+            bool be = true;
+              for(int k=1;k<=j;k++){
+                if(p[k-1]!='*') {
+                    be = false;
+                    // dp[0][j] = 0;
+                }
+            }
+            dp[0][j] = be;
+        }
+        
+        for(i=1;i<=n;i++){
+            for(j=1;j<=m;j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        
+        return dp[n][m];
     }
 };
