e46394247166726088a3120380a5d0d73b273254
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-22 20:40:01 +0530

    Time: 37 ms (16.02%), Space: 17.2 MB (14.05%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -23,25 +23,27 @@ public:
                 
         int n = prices.size();
         memset(dp,0,sizeof dp);
+        vector<int>next(2*k+1),curr(2*k+1);
         
         int i,j;
         
         for(i=n-1;i>=0;i--){
             for(int t = 2*k-1;t>=0;t--){
                 if(t%2==0){
-                    int op1 = -prices[i] + dp[i+1][t+1];
-                    int op2 = dp[i+1][t];
-                    dp[i][t] = max(op1,op2);
+                    int op1 = -prices[i] + next[t+1];
+                    int op2 = next[t];
+                    curr[t] = max(op1,op2);
                 }
                 else{
-                    int op1 = +prices[i] + dp[i+1][t+1];
-                    int op2 = dp[i+1][t];
-                    dp[i][t] = max(op1,op2); 
+                    int op1 = +prices[i] + next[t+1];
+                    int op2 = next[t];
+                    curr[t] = max(op1,op2); 
                 }
             }
+            next = curr;
         }
         
-        return dp[0][0];
+        return curr[0];
         
         
     }
