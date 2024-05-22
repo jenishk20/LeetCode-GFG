54a7a91b85f7e8e67d7edaf0e65fc275af5f88c6
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-22 18:32:19 +0530

    Time: 8 ms (29.46%), Space: 16.4 MB (24.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -27,6 +27,25 @@ public:
         int n = prices.size();
         memset(dp,-1,sizeof dp);
         
+        int i,j;
+        
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                long profit =0;
+                
+                if(buy){
+                    profit = max(-prices[i] + dp[i+1][0], dp[i+1][1]);
+                }
+                else{
+                    profit = max(prices[i] + dp[i+1][1] , dp[i+1][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
+        
         return recur(0,1,n,prices);
     }
 };
