c6e0cee32bd932cae7adde82e3a12b1cf34b33df
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-22 20:34:44 +0530

    Time: 26 ms (16.82%), Space: 17.3 MB (13.85%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int dp[1001][2][101];
+    int dp[1001][1001];
+    
+    int recur(int i,int cap,int tNo,vector<int>&prices,int n){
+        
+        if(i==n || tNo == 2*cap) return 0;
+        
+        if(dp[i][tNo]!=-1) return dp[i][tNo];
+        
+        if(tNo%2==0){
+            int op1 = -prices[i] + recur(i+1,cap,tNo+1,prices,n);
+            int op2 = recur(i+1,cap,tNo,prices,n);
+            return dp[i][tNo] = max(op1,op2);
+        }
+        else{
+            int op1 = +prices[i] + recur(i+1,cap,tNo+1,prices,n);
+            int op2 = recur(i+1,cap,tNo,prices,n);
+            return dp[i][tNo] = max(op1,op2);  
+        }
+    }
     int maxProfit(int k, vector<int>& prices) {
                 
         int n = prices.size();
-        memset(dp,0,sizeof dp);
+        memset(dp,-1,sizeof dp);
         
-        int i,j;
-    
+        return recur(0,k,0,prices,n);
         
-        for(i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap = 1;cap<=k;cap++){
-                    long profit = 0;
-                    if(buy){
-                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][!buy][cap],
-                                             dp[i+1][buy][cap]);
-                    }
-                    else{
-                        dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1],
-                                             dp[i+1][0][cap]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][k];
+        
     }
 };
