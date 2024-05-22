0ec97c8f6decf8370b16a66b0597b1c041b4d19c
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-22 21:03:31 +0530

    Time: 7 ms (26.31%), Space: 13.3 MB (98.87%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    int dp[5002][4001];
+    int dp[5002][2];
     int recur(int idx,int tNo,vector<int>&prices,int n){
         if(idx >= n) return 0;
         
         if(dp[idx][tNo]!=-1) return dp[idx][tNo];
         
-        if(tNo%2==0){
-            int op1 = -prices[idx] + recur(idx+1,tNo+1,prices,n);
-            int op2 = recur(idx+1,tNo,prices,n);
+        if(tNo){
+            int op1 = -prices[idx] + recur(idx+1,0,prices,n);
+            int op2 = recur(idx+1,1,prices,n);
             return dp[idx][tNo] = max(op2,op1);
         }
         else{
-            int op1 = prices[idx] + recur(idx+2,tNo+1,prices,n);
-            int op2 = recur(idx+1,tNo,prices,n);
+            int op1 = prices[idx] + recur(idx+2,1,prices,n);
+            int op2 = recur(idx+1,0,prices,n);
             return dp[idx][tNo] = max(op1,op2);
         }
     }
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        memset(dp,-1,sizeof dp);
-        return recur(0,0,prices,n);
+        memset(dp,0,sizeof dp);
+        
+        int i,j;
+        
+        for(i=n-1;i>=0;i--){
+            for(int t = 0;t<=1;t++)
+            {
+                if(t){
+                    int op1 = -prices[i] + dp[i+1][0];
+                    int op2 = dp[i+1][t];
+                    dp[i][t] = max(op2,op1);
+                    }
+                else{
+                    int op1 = +prices[i] + dp[i+2][1];
+                    int op2 = dp[i+1][t];
+                    dp[i][t] = max(op2,op1);
+                }
+            }
+        }
+    
+        return dp[0][1];
     }
 };
