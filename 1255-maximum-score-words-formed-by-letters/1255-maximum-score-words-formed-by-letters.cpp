c6c701f83b7f08e9b6668927e2bb7ba322be04bd
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-25 05:33:08 +0530

    Time: 44 ms (16.89%), Space: 25.4 MB (21.17%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -13,18 +13,18 @@ public:
     
     int getScore(string word,map<char,int>&scoreMap){
         int ans =0;
+        cout<<"Inside Score Map function "<<word<<endl;
         for(auto it : word){
             cout<<it<<" "<<scoreMap[it]<<endl;
             ans+=scoreMap[it];
         }
         return ans;
     }
-    int recur(int idx,vector<string>&words,map<char,int>&scoreMap,map<char,int>&
-             letterQuant){
+    int recur(int idx,vector<string>&words,map<char,int>&letterQuant,map<char,int>&scoreMap){
         
         if(idx==words.size()) return 0;
         
-        int op1 = recur(idx+1,words,scoreMap,letterQuant);
+        int op1 = recur(idx+1,words,letterQuant,scoreMap);
         int op2 = 0;
         
         if(canFormWord(words[idx],letterQuant)){
@@ -33,8 +33,8 @@ public:
                 letterQuant[it]--;
             }
             
-            cout<<words[idx]<<" "<<getScore(words[idx],scoreMap)<<endl;
-            op2 = getScore(words[idx],scoreMap) + recur(idx+1,words,scoreMap,letterQuant);    
+            op2 = getScore(words[idx],scoreMap) + recur(idx+1,words,letterQuant,scoreMap);    
+            
             for(auto it : words[idx]){
                 letterQuant[it]++;
             }
@@ -42,24 +42,8 @@ public:
         }
         return max(op1,op2);
     }
-    
-    int dfs(vector<string>& ws, vector<int> &cnt, vector<int> &score, int i) {
-    if (i >= ws.size()) return 0;
-    auto skipGain = dfs(ws, cnt, score, i + 1), gain = 0, formed = 1;
-    vector<int> cnt1(begin(cnt), end(cnt));
-    for (auto ch : ws[i]) {
-        if (--cnt1[ch - 'a'] < 0) formed = 0;
-        gain += score[ch - 'a'];
-    }
-    return max(skipGain, formed ? 
-        gain + dfs(ws, cnt1, score, i + 1) : 0);
-}
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
         
-        
-        vector<int> cnt(26);
-        for (auto ch : letters) ++cnt[ch - 'a'];
-        return dfs(words, cnt, score, 0);
         map<char,int>scoreMap;
         map<char,int>letterQuant;
         
@@ -67,7 +51,6 @@ public:
         
         for(char ch = 'a';ch<='z';ch++){
             scoreMap[ch] = score[idx++]; 
-            cout<<ch<<" "<<scoreMap[ch]<<endl;
         }
         
         for(auto it : letters){
