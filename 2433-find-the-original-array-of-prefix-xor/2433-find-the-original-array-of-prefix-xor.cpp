a837431942a6ca7bbea1544fdb8142552937fe57
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-05-31 08:17:16 +0530

    Time: 85 ms (21.90%), Space: 81.6 MB (32.63%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -3,15 +3,8 @@ public:
     vector<int> findArray(vector<int>& pref) {
         
         vector<int>ans;
-        
-        int n = pref.size();
-        int xori = 0;
-        
-        for(auto it : pref)
-            xori^=it;
-        
         ans.push_back(pref[0]);
-        xori = pref[0];
+        int xori = pref[0];
         
         for(int i=1;i<pref.size();i++){
             
