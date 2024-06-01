de0b2dd29f806e801a596352864b8ce074e30e5d
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-06-01 08:27:28 +0530

    Time: 3 ms (64.23%), Space: 8.3 MB (51.13%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-     int xorSum(vector<int>arr, int n)
-    {
-    int bits = 0;
- 
-    // Finding bitwise OR of all elements
-    for (int i=0; i < n; ++i)
-        bits |= arr[i];
- 
-    int ans = bits * pow(2, n-1);
- 
-    return ans;
-    }
     int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
         
+        int ori = 0;
         
-        return xorSum(nums,nums.size());
-       
+        for(auto it : nums){
+            ori |= it;
+        }
+        
+        return ori * pow(2,n-1);
     }
 };
