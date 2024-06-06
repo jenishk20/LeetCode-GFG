f86cf2e3c4809952ae5f026cc3cda490a4876917
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-06-06 07:45:06 +0530

    Time: 68 ms (32.25%), Space: 31.5 MB (33.43%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -7,23 +7,19 @@ public:
         if(n%groupSize!=0) return false;
         
         sort(hand.begin(),hand.end());
-        set<int>se;
+
         map<int,int>ma;
         for(auto it : hand){
             ma[it]++;
-            se.insert(it);
         }
         
         for(auto it : hand){
             
             if(ma[it]>0){
-                
                 int k = ma[it];
                 int curr = it+1;
                 int j=1;
                 while(j<groupSize){
-                    
-                    cout<<it<<" "<<curr<<" "<<ma[curr]<<endl;
                     if(ma[curr]<k){
                         return false;
                     }
@@ -32,12 +28,9 @@ public:
                     j++;
                 }
                 ma[it] = 0;
-                
             }
             
         }
-        
-      
         return true;
     }
 };
