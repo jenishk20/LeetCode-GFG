373be85c77420bb1d7f13e7378a568a00aae5858
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-06-13 22:27:26 +0530

    Time: 188 ms (86.76%), Space: 22.6 MB (98.95%) - LeetHub

--- 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int dp[2000][6000];
-    int recur(int i,int n,vector<int>&rewardValues,int x){
-        
-        if(i>=n) return 0;
-        
-        int op1 = 0;
-        int op2 = 0;
-        if(dp[i][x]!=-1){
-            return dp[i][x];
-        }
-        
-        if(x<rewardValues[i]){
-            op1 = rewardValues[i] + recur(i+1,n,rewardValues,x+rewardValues[i]);
-            op2 = recur(i+1,n,rewardValues,x);
-        }
-        else{
-            op1 = recur(i+1,n,rewardValues,x);
-        }
-        return dp[i][x] = max(op1,op2);
-    }
+   
     int maxTotalReward(vector<int>& rewardValues) {
         
-        int n = rewardValues.size();
+        bitset<100001>dp,mask,prev;
+        
         sort(rewardValues.begin(),rewardValues.end());
-        memset(dp,-1,sizeof dp);
-        return recur(0,n,rewardValues,0);
+        
+        int i;
+        dp[0] = 1;
+        
+        
+        int currentMask = 0;
+        
+        for(auto it : rewardValues){
+            while(currentMask < it) mask[currentMask++] = 1;
+           
+            prev = (dp&mask);
+            dp|=(prev<<it);
+        }
+        
+        int res = 1e5-1;
+        
+        while(!dp[res]) --res;
+        
+        return res;
+
     
        
     }
