b69299f022f9f3926ed72b5ed58e108abfd6ab40
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-06-13 19:28:19 +0530

    Time: 2827 ms (5.02%), Space: 75.3 MB (57.15%) - LeetHub

---
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    map<pair<int,int>,int>ma;
+    int dp[2000][6000];
+    int recur(int i,int n,vector<int>&rewardValues,int x){
+        
+        if(i>=n) return 0;
+        
+        int op1 = 0;
+        int op2 = 0;
+        if(dp[i][x]!=-1){
+            return dp[i][x];
+        }
+        if(x<rewardValues[i]){
+            op1 = rewardValues[i] + recur(i+1,n,rewardValues,x+rewardValues[i]);
+            op2 = recur(i+1,n,rewardValues,x);
+        }
+        else{
+            op1 = recur(i+1,n,rewardValues,x);
+        }
+        return dp[i][x] = max(op1,op2);
+    }
+    int maxTotalReward(vector<int>& rewardValues) {
+        
+        int n = rewardValues.size();
+        sort(rewardValues.begin(),rewardValues.end());
+        memset(dp,-1,sizeof dp);
+        return recur(0,n,rewardValues,0);
+    
+       
+    }
+};
