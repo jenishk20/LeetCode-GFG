449b80ec4ad9d1cb79da38e0a8dc2bade15b084d
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-06-14 05:52:48 +0530

    Time: 479 ms (6.89%), Space: 69.2 MB (24.61%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        int i;
+        int cnt = 0;
+        
+        int maxTillNow = nums[0];
+        
+        for(i=1;i<n;i++){
+            if(nums[i] <= maxTillNow)   
+            {
+                cnt += (maxTillNow - nums[i] + 1);
+                nums[i] = maxTillNow + 1;
+                
+            }
+            maxTillNow = max(maxTillNow,nums[i]);
+            cout<<maxTillNow<<" "<<cnt<<endl;
+        }
+        return cnt;
+    }
+};
