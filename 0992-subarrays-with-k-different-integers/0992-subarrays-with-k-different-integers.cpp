c0a527be226b2ec4382b6e9ae5911780e29fb0de
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-06-22 21:47:12 +0530

    Time: 755 ms (5.11%), Space: 51.1 MB (90.99%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-     int subarraysWithKDistinct(vector<int>& A, int K) {
-        return atMostK(A, K) - atMostK(A, K - 1);
-    }
-    int atMostK(vector<int>& A, int K) {
-        int i = 0, res = 0;
-        unordered_map<int, int> count;
-        for (int j = 0; j < A.size(); ++j) {
-            if (!count[A[j]]++) K--;
-            while (K < 0) {
-                if (!--count[A[i]]) K++;
-                i++;
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        map<int,int>ma;
+        
+        int l = 0,m = 0, r = 0;
+        int count = 0;
+        
+        while(r<n){
+            
+            ma[nums[r]]++;
+            
+            cout<<ma.size()<<" "<<ma[nums[l]]<<endl;
+            while(ma.size()>k){
+                ma[nums[m]]--;
+                if(ma[nums[m]]==0) ma.erase(nums[m]);
+                m++;
+                l = m;
             }
-            res += j - i + 1;
+            
+            while(ma[nums[m]]>1){
+                ma[nums[m]]--;
+                m++;
+            }
+            
+            if(ma.size() == k){
+                count+=(m-l+1);
+            }
+            
+            cout<<l<<" "<<m<<" "<<r<<" "<<count<<endl;
+    
+            r++;
         }
-        return res;
+        
+        return count;
     }
 };
