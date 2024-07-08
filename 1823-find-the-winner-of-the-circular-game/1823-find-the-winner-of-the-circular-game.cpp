164ad7314a25e3805fd533ba516dd4bb38fc702f
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-07-08 19:23:14 -0400

    Time: 0 ms (100.00%), Space: 7.2 MB (62.44%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int dp[1001];
     int recur(int n,int k){
         if(n==1) return 0;
         
+        if(dp[n]!=-1) return dp[n];
+        
         int idx = recur(n-1,k);
         idx = (idx + k)%n;
-        return idx;
+        return dp[n] = idx;
     }
     int findTheWinner(int n, int k) {
         
+        memset(dp,-1,sizeof dp);
         return 1 + recur(n,k);
     }
 };
