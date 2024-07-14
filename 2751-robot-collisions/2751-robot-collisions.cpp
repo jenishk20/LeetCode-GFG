8584def1f0612b044847be4a8fbf724186a08826
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-07-13 20:24:21 -0400

    Time: 405 ms (19.63%), Space: 230.9 MB (31.85%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,36 +1,65 @@
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& h, string directions) {
-         int n = positions.size();
-        vector<int> ind(n), stack, res;
-        for (int i = 0; i < n; i++)
-            ind[i] = i;
-        sort(ind.begin(), ind.end(), [&](int a, int b) {
-            return positions[a] < positions[b];
-        });
-        for (int i : ind) {
-            if (directions[i] == 'R') {
-                stack.push_back(i);
-                continue;
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& h, string dir) {
+     
+        vector<int>ans;
+
+        map<int,int>ma;
+
+        int n = positions.size();
+
+        int i;
+
+        for(i=0;i<n;i++){
+            ma[positions[i]] = i;
+        }
+
+        sort(positions.begin(),positions.end());
+
+        stack<int>positionTrack;
+
+        for(i=0;i<n;i++){
+            
+            int currPosition = positions[i];
+            int idxInInitialArray = ma[currPosition];
+            int healthOfCurrentRobot = h[idxInInitialArray];
+            char directionOfCurrentRobot = dir[idxInInitialArray];
+            
+            if(directionOfCurrentRobot == 'R'){
+                positionTrack.push(idxInInitialArray);
             }
-            while (!stack.empty() && h[i] > 0) {
-                if (h[stack.back()] < h[i]) {
-                    h[stack.back()] = 0, stack.pop_back();
-                    h[i] -= 1;
-                } else if (h[stack.back()] > h[i]) {
-                    h[stack.back()] -= 1;
-                    h[i] = 0;
-                } else {
-                    h[stack.back()] = 0, stack.pop_back();
-                    h[i] = 0;
+            else{
+
+                while(!positionTrack.empty() and dir[positionTrack.top()] == 'R'
+                  and h[idxInInitialArray]){
+
+                    int previousRobotIdx = positionTrack.top();
+                    positionTrack.pop();
+
+                    if(h[idxInInitialArray]  == h[previousRobotIdx]){
+                        h[previousRobotIdx] = 0;
+                        h[idxInInitialArray] = 0;
+                    }
+                    else if(h[idxInInitialArray] > h[previousRobotIdx]){
+                        h[idxInInitialArray]--;
+                        h[previousRobotIdx] = 0;
+                    }
+                    else{
+                        h[previousRobotIdx]--;
+                        h[idxInInitialArray] = 0;
+                        positionTrack.push(previousRobotIdx);
+                    }
                 }
             }
         }
-        for (int v : h) {
-            if (v > 0) {
-                res.push_back(v);
-            }
+
+        for(auto it : h){
+            if(it>0)
+            ans.push_back(it);
         }
-        return res;
+        return ans;
+
+
+
     }
 };
