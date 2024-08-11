f99e265755fc92e5b9e4fafe5c3f00750b1b1e16
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-08-11 19:44:44 -0400

    Time: 16 ms (67.19%), Space: 20.2 MB (27.05%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,68 +1,34 @@
 class Solution {
-public:
-    unordered_map<string,vector<string>>ma;
-    int n = 0;
-    vector<string>route;
 
-    void dfs(string currCity,vector<string>&tempRoute){
-
-        tempRoute.push_back(currCity);
-
-        // cout<<currCity<<endl;
-        if(tempRoute.size() == n + 1) {
+private:
+    map<string,multiset<string>>graph;
+private:
+    void dfs(string s,vector<string>&ans)
+    {
+        while(graph[s].size()!=0)
+        {
+            string res=*graph[s].begin();
+            graph[s].erase(graph[s].begin());
+            dfs(res,ans);
+            ans.push_back(res);
             
-            route = tempRoute;
-            return;
         }
-
-        vector<string>&neighBours = ma[currCity];
-
-        for(int i=0;route.empty() and i<neighBours.size();i++){
-            
-            string toAirport = neighBours[i];
-            neighBours.erase(neighBours.begin()+i);
-            dfs(toAirport, tempRoute);
-            neighBours.insert(neighBours.begin()+i, toAirport);            
-        }
-        tempRoute.pop_back();
+        
     }
+public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         
-        n = tickets.size();
-
-        if(n==0) return route;
-
-        for(auto it : tickets){
-            string u = it[0];
-            string v = it[1];
-
-            ma[u].push_back(v);
+        for(auto it:tickets)
+        {
+            graph[it[0]].insert(it[1]);
         }
-
-        for(auto &it : ma){
-            sort(it.second.begin(),it.second.end());
-        }
-
-        stack<string>se;
+       
+        vector<string>ans;
+        dfs("JFK",ans);
+        reverse(ans.begin(),ans.end());
+           ans.insert(ans.begin(),"JFK");
+        return ans;
         
-
-        se.push("JFK");
-
-        while(!se.empty()){
-
-            string currCity = se.top();
-            
-            if(ma[currCity].size() == 0){
-                route.push_back(currCity);
-                se.pop();
-            }
-            else{
-                auto dst = ma[currCity].begin();
-                se.push(*dst);
-                ma[currCity].erase(dst);
-            }
-        }
-        reverse(route.begin(),route.end());
-        return route;
+        
     }
 };
