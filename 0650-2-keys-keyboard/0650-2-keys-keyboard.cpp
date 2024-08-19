73cea01eadec83a9e3b329f039eeaca3f6deaa0b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-08-19 18:04:00 -0400

    Time: 13 ms (40.21%), Space: 12 MB (25.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+    int dp[1001][1001];
     int recur(int currA,int currC,int n){
         if(currA == n) return 0;
         if(currA > n) return 1e9;
         
+        if(dp[currA][currC]!=-1) return dp[currA][currC];
+        
         int op1 = 2 + recur(currA+currA,currA,n);
         int op2 = 1 + recur(currA+currC,currC,n);
         
-        return min(op1,op2);
+        return dp[currA][currC] = min(op1,op2);
     }
     int minSteps(int n) {
         
         if(n==1) return 0;
+        memset(dp,-1,sizeof dp);
         
         int currA = 1;
         int currC = 1;
