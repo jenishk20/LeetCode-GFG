31daa26ba9a519baa75e9c6fc4bbe07dcbd3f568
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-08-19 07:59:30 -0400

    Time: 3 ms (26.11%), Space: 7.6 MB (9.78%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        
+        if(n==0) return 0;
+        
+        while(n%2==0){
+            n/=2;
+        }
+        
+        for(int i=3;i<=n and i<=6;i+=2){
+            while(n%i==0){
+                n/=i;
+            }
+        }
+        cout<<n<<endl;
+        if(n!=1) return false;
+        
+        return true;
+    }
+};
