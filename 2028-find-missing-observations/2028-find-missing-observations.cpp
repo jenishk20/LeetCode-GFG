4d1c660406787f0106a167c3c0d406d3924056ba
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-09-06 07:20:55 +0530

    Time: 102 ms (41.12%), Space: 125.1 MB (37.96%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -4,19 +4,23 @@ public:
         int m = rolls.size();
         int sumOfExistingRolls = accumulate(rolls.begin(),rolls.end(),0);
         int unknownSum = mean*(n+m) - sumOfExistingRolls;
-
+        vector<int>res;
+        
         int maxUnknownSum = n*6;
         if(unknownSum > maxUnknownSum  || unknownSum < n) return {};
 
         if(unknownSum == maxUnknownSum) return vector<int>(n,6);
 
         if(unknownSum%n == 0) return vector<int>(n,unknownSum/n);
-
         
-        int part = unknownSum / n, rem = unknownSum % n;
-        vector<int> ans(n, part);
-        for (int i = 0; i < rem; ++i) 
-            ++ans[i];
-        return ans;
+        cout<<unknownSum<<" "<<n<<endl;
+        for(int i=n;i>=1;i--){
+            int k = min(6,unknownSum - i +1);
+            res.push_back(k);
+            unknownSum -= k;
+        }
+        
+        return res;
+
     }
 };
