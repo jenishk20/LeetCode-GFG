03101171a9d38abbcd349b86c102307ee6d5f5db
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-09-08 20:52:21 +0530

    Time: 3 ms (89.05%), Space: 14.1 MB (5.18%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        
+        int length = 0;
+        ListNode *curr = head;
+        
+        while(curr){
+            length++;
+            curr = curr->next;
+        }
+        
+        int partSize = ceil((double)length/k);
+        int timesMax = 0;
+        
+        if(length == k){
+            timesMax = k;    
+        }
+        else if((length%k)==0){
+            timesMax = k;
+        }
+        else{
+            timesMax = length%k;
+        }
+        // int timesMax = (length%k == 0)?length/k : length%k;
+        curr = head;
+        
+        cout<<length<<" "<<partSize<<" "<<timesMax<<endl;
+        vector<ListNode *>ans;
+        
+        while(k--){
+            
+            if(!curr){
+                ans.push_back(NULL);
+                continue;
+            }
+            
+            if(timesMax){
+                timesMax--;
+                
+                ListNode *temp = new ListNode(-1);
+                ListNode *head = temp;
+                for(int i=0;i<partSize;i++){
+                    temp->next = new ListNode(curr->val);
+                    curr = curr->next;
+                    temp = temp->next;
+                }
+                
+                ans.push_back(head->next);
+            }
+            else{
+                
+                ListNode *temp = new ListNode(-1);
+                ListNode *head = temp;
+                for(int i=0;i<partSize-1;i++){
+                    temp->next = new ListNode(curr->val);
+                    curr = curr->next;
+                    temp = temp->next;
+                }
+                
+                ans.push_back(head->next);
+            }
+        }
+        return ans;
+    }
+};
