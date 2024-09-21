cfceb82d5e851679d5f50cc823c4261a6feed6dd
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-09-21 18:43:48 -0400

    Time: 187 ms (5.98%), Space: 17.6 MB (32.69%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int>ans;
+    void recur(string curr,int n){
+        
+        int currRes = stoi(curr);
+        if(currRes > n) return;
+        
+        cout<<curr<<endl;
+        ans.push_back(currRes);
+        for(int i=0;i<=9;i++){
+            recur(curr + to_string(i),n);
+            
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+        
+        for(int i=1;i<=9;i++){
+            recur(to_string(i),n);
+        }
+        
+        return ans;
+    }
+};
