405e4135447d3265a95bce7ac045cdcb6132de12
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-09-23 11:27:13 -0400

    Time: 53 ms (100.00%), Space: 28.3 MB (100.00%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -4,25 +4,27 @@ public:
 
     ll calc(ll workerTimes,ll haveTime){
         
-        ll ans = 0;
-        ll low = 1;
-        ll high = 1e6;
-        
+       
         haveTime *= 2;
         haveTime /= workerTimes;
         
-        while(low<=high){
-            ll mid = (low+high)/2;
-            
-            if((mid*1ll*(mid+1))<=haveTime){
-                ans = mid;
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return ans;
+        double det = 1 + 4*haveTime;
+        
+        double d1 = -1 + sqrt(det) / 2.0;
+        double d2 = -1 - sqrt(det) / 2.0;
+        
+        // cout<<haveTime<<" "<<det<<" "<<d1<<" "<<d2<<endl;
+        
+        d1 = ceil(d1);
+        
+        if(d1*d1 + d1 - haveTime <= 0) return d1;
+        else return --d1;
+        
+        d2 = ceil(d2);
+        
+        if(d2*d2 + d2 - haveTime <= 0) return d2;
+        else return --d2;
+      
     }
     bool check(ll time,vector<int>&workerTimes,int mountainHeight){
         
@@ -32,6 +34,8 @@ public:
         for(int i=0;i<workerTimes.size();i++){
             ll haveTime = time;
             ll findMaxMul = calc(workerTimes[i],haveTime);
+            
+            // cout<<"HERE "<<workerTimes[i]<<" "<<findMaxMul<<endl;
             done += findMaxMul;
         }
         
