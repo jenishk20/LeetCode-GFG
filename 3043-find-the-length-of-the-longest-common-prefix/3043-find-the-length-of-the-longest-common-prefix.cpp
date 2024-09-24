cb907df4ccbd7afbdd9fb616134fd541db65c064
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-09-24 18:23:44 -0400

    Time: 385 ms (46.02%), Space: 169.5 MB (44.91%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,95 @@
+class Node{
+public:
+    
+    Node *links[11];
+    bool isEnd;
+    
+    Node(){
+        for(int i=0;i<=10;i++){
+            links[i] = NULL;
+        }
+        isEnd = false;
+    }
+    
+    void put(char ch,Node *node){
+        
+        links[ch-'0'] = node;
+    }
+    
+    bool containsKey(char ch){
+        return links[ch-'0'] != NULL;
+    }
+    
+    Node *next(char ch){
+        return links[ch-'0'];
+    }
+    
+};
+
+class Trie{
+    
+public:
+    Node *root;
+    
+    Trie(){
+        root = new Node();    
+    }
+    
+    void insert(int x){
+        
+        string s = to_string(x);
+        
+        cout<<x<<endl;
+        Node *curr = root;
+        
+        for(int i=0;i<s.size();i++){
+            if(!curr->containsKey(s[i])){
+                curr->put(s[i],new Node());
+            }
+            curr = curr->next(s[i]);
+        }
+    }
+    
+    int calcMaxPrefix(int x){
+        
+        int cnt = 0;
+        string s = to_string(x);
+        
+        Node *curr = root;
+        
+        for(int i=0;i<s.size();i++){
+            
+            // cout<<s[i]<<" "<<curr->containsKey(s[i])<<endl;
+            if(!curr->containsKey(s[i])){
+                return cnt;
+            }
+            curr = curr->next(s[i]);
+            cnt++;
+        }
+        return cnt;
+    }
+    
+};
+
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        
+        Trie *trie = new Trie();
+        
+        for(auto it : arr1){
+            trie->insert(it);
+        }
+        
+        int maxi = 0;
+        
+        for(auto it : arr2){
+            
+            int ans = trie->calcMaxPrefix(it);
+            maxi = max(maxi,ans);
+        }
+        
+        return maxi;
+    }
+};
