e4e805051fe3d8161e00ae12d697c5289548c9e7
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2024-11-22 18:44:27 -0500

    Time: 68 ms (30.28%), Space: 75.4 MB (58.05%) - LeetHub

---
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        map<string,int>ma;
+        
+        for(int i=0;i<n;i++){
+            char st = matrix[i][0];
+            string res = "B";
+            for(int j=1;j<m;j++){
+                if(matrix[i][j] == st){
+                    res += "B";
+                }
+                else{
+                    res += "C";
+                }
+            }
+            cout<<res<<endl;
+            ma[res]++;
+        }
+        
+        int maxi = 0;
+        for(auto it : ma){
+            maxi = max(maxi,it.second);
+        }
+        
+        return maxi;
+    }
+};
