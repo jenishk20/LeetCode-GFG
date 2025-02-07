a0d73294cd204ee112e13ff25c176a77491fe8fe
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-02-07 16:46:42 -0500

    Time: 7 ms (36.51%), Space: 13.9 MB (62.73%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,49 +1,44 @@
 class Solution {
 public:
+int findPairs(int mid,vector<int>&nums){
+    int j=1;
+    int i=0;
+    int cnt = 0;
+    int n = nums.size();
 
-    int countPairs(int mid,vector<int>&nums){
-
-        int i = 0;
-        int j = 1;
-        int n = nums.size();
-
-        int cnt = 0;
-
-        while(j<n){
-            if(nums[j]-nums[i]<=mid){
-                cnt += j-i;
-            }
-            else{
-                while(i<j and nums[j]-nums[i]>mid){
-                    i++;
-                }
-                continue;
-            }
-            j++;
+    while(j<n){
+        if(nums[j]-nums[i]<=mid){
+            cnt+=(j-i);
         }
-        return cnt;
+        else{
+            while(i<j and nums[j]-nums[i]>mid){
+                i++;
+            }
+            cnt += (j-i);
+        }
+        j++;
     }
+    return cnt;
+}
     int smallestDistancePair(vector<int>& nums, int k) {
-        int n = nums.size();
-        int i,j;    
-
         sort(nums.begin(),nums.end());
-
+        int n = nums.size();
+        int ans = 0;
         int low = 0;
         int high = nums[n-1];
-        int ans = 0;
-
         while(low<=high){
             int mid = (low+high)/2;
 
-            int pairs = countPairs(mid,nums);
-            if(pairs >= k){
+            int noOfPairs = findPairs(mid,nums);
+            cout<<mid<<" "<<noOfPairs<<endl;
+            if(noOfPairs>=k){
                 ans = mid;
                 high = mid-1;
             }
-            else low = mid+1;
+            else{
+                low = mid+1;
+            }
         }
-
         return ans;
     }
 };
