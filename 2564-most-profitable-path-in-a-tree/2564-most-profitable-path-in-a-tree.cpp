821b71c3235fea984a3a676e04de03d1cd8d3610
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-02-24 17:36:22 -0500

    Time: 2676 ms (5.08%), Space: 155.4 MB (68.85%) - LeetHub

--- 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
@@ -1,66 +1,45 @@
 class Solution {
 public:
-    map<int,int>cameFrom;
-    vector<int>reachTime;
-    int res = -1e9;
-    void dfs(int node,int parent,vector<int>graph[],int stTime,vector<int>&vis){
-        cameFrom[node] = parent;
-        vis[node] = 1;
-        reachTime[node] = stTime;
+    int dfs(int node,int parent,int currTime,vector<int>&bobDistance,vector<int>&amount,vector<int>graph[],int bobNode){
+
+        int maxI = 0;
+        int maxCh = -1e9;
+
+        if(node == bobNode) bobDistance[node] = 0;
+        else bobDistance[node] = 1e8;
+
         for(auto it : graph[node]){
-            if(!vis[it]){
-                dfs(it,node,graph,stTime+1,vis);
+            if(it != parent){
+                maxCh = max(maxCh,dfs(it,node,currTime+1,bobDistance,amount,graph,bobNode));
+                bobDistance[node] = min(bobDistance[node],bobDistance[it]+1);
             }
         }
-    }
+        if(bobDistance[node] > currTime){
+            maxI += amount[node];
+        }
+        else if(bobDistance[node] == currTime){
+            maxI += (amount[node]/=2);
+        }
+        cout<<node<<" "<<currTime<<" "<<maxCh<<" "<<maxI<<" "<<bobDistance[node]<<endl;
 
-    void dfs1(int node,vector<int>graph[],vector<int>&bobPath,vector<int>&amount,int currTime,int ans,vector<int>&vis){
 
-        vis[node] = 1;
-        if(bobPath[node] > currTime || bobPath[node] == -1){
-            ans += amount[node];
-        }
-        if(bobPath[node] == currTime){
-            ans += amount[node]/2;
-        }
-        bool found = false;
-        for(auto it : graph[node]){
-            if(!vis[it]){
-                found = true;
-                dfs1(it,graph,bobPath,amount,currTime+1,ans,vis);
-            }
-        }
-        if(found == false) 
-        res = max(res,ans);
+        if(maxCh == -1e9) return maxI;
+        return maxI  + maxCh;
     }
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+        
+        int n = edges.size();
+        vector<int>graph[n+1];
 
-            int n = edges.size();
-            vector<int>graph[n+1];
-            vector<int>vis(n+1,0);
-            reachTime.resize(n+1,0);
-            vector<int>bobPath(n+1,-1);
+        for(auto it : edges){
+            int u = it[0];
+            int v = it[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
 
-            for(auto it : edges){
-                int u = it[0];
-                int v = it[1];
-                graph[u].push_back(v);
-                graph[v].push_back(u);
-            }
-
-            cameFrom[bob] = -1;
-            dfs(bob,-1,graph,0,vis);
-
-            int st = 0;
-            bobPath[bob] = 0;
-            while(st!=bob){
-                bobPath[st] = reachTime[st];
-                st = cameFrom[st];
-            }
-
-            for(int i=0;i<=n;i++) vis[i] = 0;
-            dfs1(0,graph,bobPath,amount,0,0,vis);
-            return res;
-
+        int stTime = 0;
+        vector<int>bobDistance(n+1,1e9);
+        return dfs(0,0,stTime,bobDistance,amount,graph,bob);
     }
 };
