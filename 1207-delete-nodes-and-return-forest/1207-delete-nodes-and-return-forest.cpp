41e368651c3c5e6e95bda17a631ced8f3a26119e
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-02-26 11:18:47 -0500

    Time: 16 ms (54.95%), Space: 27 MB (47.85%) - LeetHub

---
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode *>ans;
+    void recur(TreeNode *root, TreeNode *parent, vector<int>&to_delete){
+        if(!root) return;
+
+        recur(root->left,root,to_delete);
+        recur(root->right,root,to_delete);
+        // cout<<root->val<<" "<<(parent != NULL? parent->val : -1)<<endl;
+
+        if(find(to_delete.begin(),to_delete.end(),root->val)!=to_delete.end()){
+            // if(!parent)
+            if(parent and parent->left == root) parent->left = NULL;
+            if(parent and parent->right == root) parent->right = NULL;
+            if(root->left) ans.push_back(root->left);
+            if(root->right) ans.push_back(root->right);
+            return;
+        }
+        if(!parent) ans.push_back(root);
+
+    }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        recur(root,NULL,to_delete);
+        return ans;
+    }
+};
