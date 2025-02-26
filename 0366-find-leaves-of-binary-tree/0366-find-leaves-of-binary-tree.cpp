94916ebe6b5cf516de9beb41e7b029f4a4fec381
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-02-25 22:27:54 -0500

    Time: 4 ms (2.35%), Space: 11.8 MB (15.49%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.cpp
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.cpp
@@ -11,33 +11,24 @@
  */
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void recur(vector<int>&temp,TreeNode *root, TreeNode *parent){
-        if(!root) return;
-        if(!root->left and !root->right){
-            if(parent == NULL){
-                temp.push_back(root->val);
-                return;
-            }
-            if(parent->left == root) parent->left = NULL;
-            if(parent->right == root) parent->right = NULL;
-            temp.push_back(root->val);
-            return;
-        } 
-        recur(temp,root->left,root);
-        recur(temp,root->right,root);
+    map<int,vector<int>>ma;
+    int recur(TreeNode *root){
+        if(!root) return 0;
+        int lh = recur(root->left);
+        int rh = recur(root->right);
+
+        int myIndex = max(lh,rh) + 1;
+        ma[myIndex].push_back(root->val);
+        return myIndex;
     }
-    vector<vector<int>> findLeaves(TreeNode* root,TreeNode *parent = NULL) {
+    vector<vector<int>> findLeaves(TreeNode* root) {
 
-        while(root->left or root->right){
-            vector<int>temp;
-
-            cout<<root->val<<endl;
-            recur(temp,root,NULL);
-            ans.push_back(temp);
-        }
+        vector<vector<int>>ans;
         
-        if(!root->left and !root->right) ans.push_back({root->val});
+        recur(root);
+
+        for(auto it : ma) ans.push_back(it.second);
         return ans;
+
     }
 };
