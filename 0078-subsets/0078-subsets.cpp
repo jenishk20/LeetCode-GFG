42a751bec346145ed42acb278cf01e403893bebb
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-02-27 07:55:23 -0500

    Time: 0 ms (100%), Space: 10.1 MB (46.4%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -12,9 +12,19 @@ public:
         recur(idx+1,nums,temp,ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>temp;
+        int n= nums.size();
         vector<vector<int>>ans;
-        recur(0,nums,temp,ans);
+
+        for(int i=0;i<pow(2,n);i++){
+            vector<int>temp;
+
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
         return ans;
     }
 };
