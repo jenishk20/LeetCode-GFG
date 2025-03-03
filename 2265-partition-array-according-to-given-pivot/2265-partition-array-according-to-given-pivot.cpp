4629a0723852cc016bcd7153a19b57d4ce7c1d8b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-03 08:55:48 -0500

    Time: 13 ms (18.1%), Space: 127.5 MB (96.86%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -5,21 +5,25 @@ public:
         int cntLess = 0,cntEq = 0;
         vector<int>res(n,0);
 
-        int i;
-        for(i=0;i<n;i++){
-            if(nums[i]<pivot) cntLess++;
-            else if(nums[i] == pivot) cntEq++;
+        int i=0;
+        int j=n-1;
+        int aSt = 0;
+        int aEn = n-1;
+        while(i<n and j>=0){
+            if(nums[i]<pivot){
+                res[aSt++] = nums[i];
+            }
+            if(nums[j]>pivot){
+                res[aEn--] = nums[j];
+            }
+            i++;
+            j--;
         }
-
-        int st = 0;
-        int mid = cntLess;
-        int next = cntLess + cntEq;
-
-        for(i=0;i<n;i++){
-            if(nums[i]<pivot) res[st++] = nums[i];
-            else if(nums[i] == pivot) res[mid++] = nums[i];
-            else res[next++] = nums[i];
+      
+        while(aSt<=aEn){
+            res[aSt++] = pivot;
         }
         return res;
+
     }
 };
