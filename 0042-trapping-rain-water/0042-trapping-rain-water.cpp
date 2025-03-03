f69e4fa32fce7c8fb237dff0deecac8445e3fd7e
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-03 13:37:47 -0500

    Time: 0 ms (100%), Space: 25.9 MB (80.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
         
-        vector<int>pfx(n,0),sfx(n,0);
-        int i;
-        pfx[0]=height[0];
-        for(i=1;i<n;i++)
-        {
-            pfx[i]=max(pfx[i-1],height[i]);
+        int lMax = 0, rMax = 0;
+        int i = 0;
+        int j = height.size()-1;
+
+        int total = 0;
+
+        while(i<j){
+            if(height[i]<=height[j]){
+                if(lMax >= height[i]){
+                    total += (lMax - height[i]);
+                }
+                else{
+                    lMax = height[i];
+                }
+                i++;
+            }
+            else{
+                if(rMax >= height[j]){
+                    total += (rMax - height[j]);
+                }
+                else{
+                    rMax = height[j];
+                }
+                j--;
+            }
         }
-        sfx[n-1]=height[n-1];
-        for(i=n-2;i>=0;i--)
-        {
-            sfx[i]=max(sfx[i+1],height[i]);
-        }
-        int ans=0;
-        for(i=0;i<n;i++)
-        {
-            int tt=min(pfx[i],sfx[i])-height[i];
-            ans+=tt;
-        }
-        return ans;
-        
+
+        return total;
     }
 };
