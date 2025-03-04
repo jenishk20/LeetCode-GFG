ad4ec3041893f1426876949d76b906dc17da95b9
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-04 10:13:54 -0500

    Time: 2 ms (53.62%), Space: 10.4 MB (11.79%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        
+        int x = 0;
+        int y = 0;
+
+        for(auto it : moves){
+            if(it == 'U') y--;
+            else if(it == 'D') y++;
+            else if(it == 'L') x--;
+            else x++;
+        }
+        return x==0 and y==0;
+    }
+};
