28d1e568633f645d4b866fdb420032768881efdb
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-04 08:18:25 -0500

    Time: 131 ms (13.85%), Space: 61.8 MB (8.37%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double>ans;
+        
+        vector<double>medians;
         int n = nums.size();
+
         multiset<int>m1,m2;
 
         for(int i=0;i<n;i++){
+
             m1.insert(nums[i]);
             m2.insert(*m1.rbegin());
             m1.erase(m1.find(*m1.rbegin()));
 
-            if(m1.size() < m2.size()){
+            int k1 = m1.size();
+            int k2 = m2.size();
+
+            if(k1<k2){
                 m1.insert(*m2.begin());
                 m2.erase(m2.find(*m2.begin()));
             }
 
             if(i>=k-1){
                 if(k&1){
-                    double res = *m1.rbegin();
-                    ans.push_back(res);
+                    medians.push_back(*m1.rbegin());
                 }
                 else{
-                    double res = (double)((double)*m1.rbegin() + *m2.begin())/2.0;
-                    ans.push_back(res);
+                    double r1 = *m1.rbegin();
+                    double r2 = *m2.begin();
+                    medians.push_back((r1 + r2)/2.0);
                 }
 
-                if(nums[i-k+1]<= *m1.rbegin()){
+                if(nums[i-k+1]<=*m1.rbegin()){
                     m1.erase(m1.find(nums[i-k+1]));
                 }
                 else{
@@ -33,6 +39,8 @@ public:
                 }
             }
         }
-        return ans;
+        return medians;
+
+
     }
 };
