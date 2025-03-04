2d328762b31a1b7e27d99f6005b7dd285ceaf9b6
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-04 14:21:55 -0500

    Time: 16 ms (10%), Space: 22.1 MB (17.2%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,34 +1,26 @@
 class Solution {
-
-private:
-    map<string,multiset<string>>graph;
-private:
-    void dfs(string s,vector<string>&ans)
-    {
-        while(graph[s].size()!=0)
-        {
-            string res=*graph[s].begin();
-            graph[s].erase(graph[s].begin());
-            dfs(res,ans);
-            ans.push_back(res);
-            
-        }
-        
-    }
 public:
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-        
-        for(auto it:tickets)
-        {
-            graph[it[0]].insert(it[1]);
+    map<string,multiset<string>>graph;
+    vector<string>ans;
+    void dfs(string curr){
+
+        while(graph[curr].size() != 0){
+            string nextToVisit = *graph[curr].begin();
+            graph[curr].erase(graph[curr].find(nextToVisit));
+            dfs(nextToVisit);
+            ans.push_back(nextToVisit);
         }
-       
-        vector<string>ans;
-        dfs("JFK",ans);
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for(auto it : tickets){
+            string u = it[0];
+            string v = it[1];
+            graph[u].insert(v);
+        }
+
+        dfs("JFK");
         reverse(ans.begin(),ans.end());
-           ans.insert(ans.begin(),"JFK");
+        ans.insert(ans.begin(),"JFK");
         return ans;
-        
-        
     }
 };
