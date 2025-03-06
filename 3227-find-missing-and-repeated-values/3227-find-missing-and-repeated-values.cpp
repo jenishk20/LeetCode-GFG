85675db4711ee2c7c539efd927d383de44c26ffd
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-06 10:02:21 -0500

    Time: 1 ms (80.71%), Space: 25.6 MB (91.87%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        long long sumOfFirstN = n*n*(n*n+1)/2;
+        long long sumOfSquareFirstN = n*1ll*n*(n*1ll*n+1)*(2*1ll*n*1ll*n+1)/6;
+
+        long gridSum = 0;
+        long gridSumSquare = 0;
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                gridSum += grid[i][j];
+                gridSumSquare += (grid[i][j] * grid[i][j]);
+            }
+        }
+
+        long diffOf2 = gridSum - sumOfFirstN;
+        long diffOfSquare = gridSumSquare - sumOfSquareFirstN;
+
+        long o1 = diffOfSquare / diffOf2;
+
+
+        int a = (o1 + diffOf2) / 2;
+        int b = (o1 - diffOf2) / 2;
+
+        return {a,b};
+
+    }
+};
