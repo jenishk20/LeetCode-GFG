d132e0dd1fc80ff2cb6a36633a288017bd9a6f9a
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-07 18:52:37 -0500

    Time: 2 ms (24.84%), Space: 30.2 MB (19.16%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -19,12 +19,7 @@ public:
                 ending = i;
             }
         }
-      
-        for(int i=0;i<n;i++){
-            cout<<dp[i]<<" ";
-        }
-        cout<<endl;
-
+    
         int exp = maxLength;
         for(int j=n-1;j>=0;j--){
             if(dp[j] == exp){
