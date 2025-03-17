841011c66bddd953314882d1d9ffbf1631d20311
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-17 13:33:38 -0400

    Time: 3240 ms (0%), Space: 213.2 MB (100%) - LeetHub

--- 3750-closest-equal-element-queries/3750-closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/3750-closest-equal-element-queries.cpp
@@ -1,68 +1,49 @@
 class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-
-        int n = nums.size();
-        map<int,vector<int>>ma;
+        
         vector<int>res;
 
+        int n = nums.size();
+        vector<int>dis(n,1e9);
+
         int i,j;
+        map<int,int>seen;
+
+        for(i=0;i<2*n;i++){
+            if(seen.count(nums[i%n])){
+                dis[i%n] = min(dis[i%n],(i) - seen[nums[i%n]]);
+            }
+            seen[nums[i%n]] = i%n;
+        }
+
+        seen.clear();
+
+        cout<<seen.size()<<endl;
 
         for(i=0;i<n;i++){
-            ma[nums[i]].push_back(i);
+            cout<<dis[i]<<" ";
+        }
+        cout<<endl;
+
+        for(i=2*n-1;i>=0;i--){
+            if(seen.count(nums[i%n])){
+                dis[i%n] = min(dis[i%n],-(i) + seen[nums[i%n]]);
+            }
+            seen[nums[i%n]] = i;
         }
 
-        for(auto &it : queries){
-            vector<int>&indices = ma[nums[it]];
-            int ans = 1e9;
-            int sz = indices.size();
+        for(i=0;i<n;i++){
+            cout<<dis[i]<<" ";
+        }
+        cout<<endl;
 
-            if(sz == 1){
-                res.push_back(-1);
-            }
-            else{
-                int idx = lower_bound(indices.begin(),indices.end(),it) - indices.begin();
-                if(idx + 1 < sz){
-                    ans = min({ans,indices[idx+1] - it });
-                }
-                else{
-                    ans = min({ans,n - it + indices[0]});
-                }
-
-                if(idx-1>=0){
-                    ans = min({ans,-indices[idx-1] + it });
-                }
-                else{
-                    ans = min({ans, it + n - indices[sz-1]});
-                }
-                res.push_back(ans);
-            }
+        for(auto it : queries){
+            if(dis[it]>=n) res.push_back(-1);
+            else res.push_back(dis[it]);
         }
 
-        // for(auto &it : queries){
-        //     int elem = nums[it];
-        //     auto vec = ma[elem];
-        //     if(vec.size() == 1){
-        //         res[i++] = -1;
-        //     }
-        //     else{
-        //         int ans = 1e9;
-        //         int firstOccurence = lower_bound(vec.begin(),vec.end(),it) - vec.begin();
-        //         if(firstOccurence+1<vec.size()){
-        //             ans = min({ans,vec[firstOccurence+1] - it});
-        //         }
-        //         else{
-        //             ans = min({ans,n-it+vec[0]});
-        //         }
-        //         if(firstOccurence-1>=0){
-        //             ans = min({ans,it - vec[firstOccurence-1]});
-        //         }
-        //         else{
-        //             ans = min({ans,it  + n - vec[vec.size()-1]});
-        //         }
-        //         res[i++] = ans;
-        //     }
-        // }
+
         return res;
     }
 };
