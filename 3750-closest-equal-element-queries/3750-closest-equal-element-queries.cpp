86aa7e8050659bbec11b44c991e910ccce1f9113
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-17 13:00:12 -0400

    Time: 396 ms (0%), Space: 219.1 MB (100%) - LeetHub

--- 3750-closest-equal-element-queries/3750-closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/3750-closest-equal-element-queries.cpp
@@ -1,44 +1,68 @@
 class Solution {
 public:
-    int find(int i, int j, int n){
-        int temp = abs(i-j);
-        return min(temp, n - temp);
-    }
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+
         int n = nums.size();
-        unordered_map<int, vector<int>> mp;
-        for(int i = 0; i<n; i++){
-            mp[nums[i]].push_back(i);
+        map<int,vector<int>>ma;
+        vector<int>res;
+
+        int i,j;
+
+        for(i=0;i<n;i++){
+            ma[nums[i]].push_back(i);
         }
 
-        vector<int>ans;
-        for(int i = 0; i<queries.size(); i++){
-            vector<int>&indices = mp[nums[queries[i]]];
-            if(indices.size()==1) ans.push_back(-1);
+        for(auto &it : queries){
+            vector<int>&indices = ma[nums[it]];
+            int ans = 1e9;
+            int sz = indices.size();
+
+            if(sz == 1){
+                res.push_back(-1);
+            }
             else{
-                int ans_Ind = INT_MAX; 
-                int ind = lower_bound(indices.begin(), indices.end(), queries[i]) - indices.begin();
-                int m = indices.size();
-                if(ind != 0){
-                    int prev = indices[ind - 1];
-                    ans_Ind = min(ans_Ind, find(queries[i], prev,n));
+                int idx = lower_bound(indices.begin(),indices.end(),it) - indices.begin();
+                if(idx + 1 < sz){
+                    ans = min({ans,indices[idx+1] - it });
                 }
                 else{
-                    int last = indices[m-1];
-                    ans_Ind= min(ans_Ind, find(queries[i], last, n));
+                    ans = min({ans,n - it + indices[0]});
                 }
 
-                if(ind + 1 != m){
-                    int next = indices[ind + 1];
-                    ans_Ind = min(ans_Ind, find(queries[i], next, n));
+                if(idx-1>=0){
+                    ans = min({ans,-indices[idx-1] + it });
                 }
                 else{
-                    int next = indices[0];
-                    ans_Ind = min(ans_Ind, find(queries[i], next, n));
+                    ans = min({ans, it + n - indices[sz-1]});
                 }
-                ans.push_back(ans_Ind);
+                res.push_back(ans);
             }
         }
-        return ans;
+
+        // for(auto &it : queries){
+        //     int elem = nums[it];
+        //     auto vec = ma[elem];
+        //     if(vec.size() == 1){
+        //         res[i++] = -1;
+        //     }
+        //     else{
+        //         int ans = 1e9;
+        //         int firstOccurence = lower_bound(vec.begin(),vec.end(),it) - vec.begin();
+        //         if(firstOccurence+1<vec.size()){
+        //             ans = min({ans,vec[firstOccurence+1] - it});
+        //         }
+        //         else{
+        //             ans = min({ans,n-it+vec[0]});
+        //         }
+        //         if(firstOccurence-1>=0){
+        //             ans = min({ans,it - vec[firstOccurence-1]});
+        //         }
+        //         else{
+        //             ans = min({ans,it  + n - vec[vec.size()-1]});
+        //         }
+        //         res[i++] = ans;
+        //     }
+        // }
+        return res;
     }
 };
