a3a1e5327e34fa51a75b37496e735c0aa6c01a5b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-21 20:19:55 -0400

    Time: 1946 ms (5.08%), Space: 124 MB (88.88%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -0,0 +1,64 @@
+class DSU{
+    public:
+    vector<int>parent,nodesInComp, edgesInComp;
+    DSU(int n){
+
+        parent.resize(n);
+        nodesInComp.resize(n,1);
+        edgesInComp.resize(n,0);
+
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void merge(int u,int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if(pu == pv){
+            edgesInComp[pv]++;
+            cout<<u<<" "<<v<<" "<<pu<<" "<<pv<<" "<<edgesInComp[pu]<<" "<<edgesInComp[pv]<<endl;
+            return;
+        }
+
+        parent[pu] = pv;
+        edgesInComp[pv] += edgesInComp[pu] + 1;
+        nodesInComp[pv] += nodesInComp[pu];
+
+        cout<<u<<" "<<v<<" "<<pu<<" "<<pv<<" "<<edgesInComp[pu]<<" "<<edgesInComp[pv]<<endl;
+    }
+
+    pair<int,int>findResult(int u){
+        return {edgesInComp[u],nodesInComp[u]};
+    }
+};
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        
+        DSU dsu(n);
+
+        for(auto it : edges){
+            int u = it[0];
+            int v = it[1];
+            dsu.merge(u,v);
+        }
+
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(dsu.parent[i] == i){
+                cout<<i<<endl;
+                auto res = dsu.findResult(i);
+                cout<<res.first<<" "<<res.second<<endl;
+                cnt += (res.second*(res.second-1) == 2*res.first);
+            }
+        }
+        return cnt;
+    }
+};
