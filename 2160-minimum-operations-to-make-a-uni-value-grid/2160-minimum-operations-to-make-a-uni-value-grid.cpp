e820773fb1e289cf27c070d699c93c74cdfd740c
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-26 11:47:07 -0400

    Time: 40 ms (64.13%), Space: 90 MB (48.02%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -5,21 +5,15 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int i,j;
-        int sum = 0;
-        int gcd = 0;
-
         vector<int>temp;
 
         for(i=0;i<n;i++){
             for(j=0;j<m;j++){
-                sum += grid[i][j];
-                gcd = __gcd(grid[i][j],gcd);
                 temp.push_back(grid[i][j]);
             }
         }
 
         sort(temp.begin(),temp.end());
-
         int sz = temp.size();
         int mid = temp[sz/2];
         int ans = 0;
