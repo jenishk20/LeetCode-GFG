53bc5a0c623358fd698a75bac239e76ae9622492
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-29 22:19:56 -0400

    Time: 15 ms (7%), Space: 9.1 MB (15.55%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        
+        int n = s.size();
+        map<char,pair<int,int>>ma;
+        
+        int i;
+        for(i=0;i<n;i++){
+            if(ma.find(s[i]) == ma.end()){
+                ma[s[i]] = {i,i};
+            }
+            else{
+                ma[s[i]] = {ma[s[i]].first,i};
+            }
+        }
+
+        for(auto it : ma){
+            cout<<it.first<<" "<<it.second.first<<" "<<it.second.second<<endl;
+        }
+
+        vector<int>ans;
+        int st = 0;
+        int prevSt = ma[s[0]].first;
+        int prevEn = ma[s[0]].second;
+        int currSt, currEn;
+        for(i=1;i<n;i++){
+            currSt = ma[s[i]].first;
+            currEn = ma[s[i]].second;
+
+            if(currSt > prevEn){
+                ans.push_back(currSt - prevSt);
+                prevSt = currSt;
+                prevEn = currEn;
+            }
+            else{
+                prevEn = max(currEn,prevEn);
+            }
+        }
+        ans.push_back(n - prevSt);
+        return ans;
+    }
+};
