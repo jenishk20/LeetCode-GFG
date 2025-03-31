b73702775911aa5c165decafa6689b13faa38713
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-03-31 16:01:40 -0400

    Time: 0 ms (100%), Space: 74.7 MB (5.08%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,19 +3,21 @@ public:
     int maxSubArray(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int>pfxSum(n+1,0);
-        pfxSum[0] = nums[0];
+        int i;
+        vector<int>pfx(n,0);
+        pfx[0] = nums[0];
+
+        for(i=1;i<n;i++){
+            pfx[i] = pfx[i-1] + nums[i];
+        }
+
         int minSoFar = 0;
         int ans = -1e9;
 
-        for(int i = 1;i<n;i++){
-            pfxSum[i] = pfxSum[i-1] + nums[i];
-        }
-
-        for(int i=0;i<n;i++){
-            int endingHereMaxSum = pfxSum[i] - minSoFar;
-            minSoFar = min(minSoFar,pfxSum[i]);
-            ans = max(ans,endingHereMaxSum);
+        for(i=0;i<n;i++){
+            int bestEndingHere = pfx[i] - minSoFar;
+            ans = max(ans,bestEndingHere);
+            minSoFar = min(minSoFar,pfx[i]);
         }
         return ans;
     }
