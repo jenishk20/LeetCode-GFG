f62f8be496201555457bd7d802d8c92da6dd278b
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-04-01 12:39:42 -0400

    Time: 999 ms (5.22%), Space: 119.8 MB (74.65%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        
+        int n = questions.size();
+        vector<long long>dp(n+1,0);
+        long long ans = 0;
+
+        for(int i=n-1;i>=0;i--){
+            long long canGet = questions[i][0];
+            long long toSkip = questions[i][1];
+            if(i+toSkip+1<n){
+                cout<<i<<" "<<toSkip<<endl;
+                dp[i] = max(dp[i+1], canGet + dp[i+toSkip+1]);
+            }
+            else{
+                dp[i] = max(dp[i+1],canGet);
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
