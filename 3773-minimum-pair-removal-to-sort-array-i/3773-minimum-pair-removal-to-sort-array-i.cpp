97a8e26083a1be67b8b469e99b8300716f948eb5
Jenish Kothari <60069162+jenishk20@users.noreply.github.com>
2025-04-08 15:10:58 -0400

    Time: 281 ms (5.08%), Space: 34.1 MB (5.58%) - LeetHub

---
+++ 3773-minimum-pair-removal-to-sort-array-i/3773-minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+       
+        int cnt = 0;
+
+        while(1){
+
+            for(auto it : nums){
+                cout<<it<<" ";
+            }
+            cout<<endl;
+            if(is_sorted(nums.begin(),nums.end())){
+                return cnt;
+            }
+            int n = nums.size();
+            vector<pair<int,int>>pairSums;
+            for(int i=0;i<n-1;i++){
+                pairSums.push_back({i,nums[i]+nums[i+1]});
+            }
+
+            sort(pairSums.begin(),pairSums.end(),[](pair<int,int>&a,pair<int,int>&b){
+
+                if(a.second == b.second) return a.first < b.first;
+                return a.second < b.second;
+            });
+
+            int minSumIdx = pairSums[0].first;
+            int sum = pairSums[0].second;
+
+            vector<int>temp;
+            
+            for(int i=0;i<n;i++){
+                if(i == minSumIdx){
+                    temp.push_back(sum);
+                    i++;
+                }
+                else{
+                    temp.push_back(nums[i]);
+                }
+            }
+            nums = temp;
+            cnt++;
+        }
+
+        return cnt;
+    }
+};
